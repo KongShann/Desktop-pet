b20db166d878304ff46ea198dc864fab8585d1a7
KongShann <kshan_xin@outlook.com>
2023-04-04 21:00:28 +0800

    实现宠物外观选择
    Signed-off-by: KongShann <kshan_xin@outlook.com>

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,9 @@
 #include <QLabel>
 #include <QPushButton>
 
+#include "petobjects_struct.h"
+#include "appchoosewindow.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -13,15 +16,30 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+public slots:
+    void OnAppearanceChanged(int label_index);
+    void OnAppChooseBtnClicked();
+
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
     void SelectAppearance(); //切换外观
+
+    void InitButton();
+
 private:
     Ui::MainWindow *ui;
 
-    QLabel* pet_appearance; //外观显示Lable指针
+    QVector<PetAppearance> *owned_pet_appearances;
+    PetAppearance displayed_pet_appearance;
 
-    QPushButton* select_appearance_btn; //切换外观按钮指针
+    QLabel* pet_displayed_label; //外观显示Lable指针
+
+    QPushButton* appchoose_btn; //切换外观按钮指针
+
+    AppChooseWindow* appchoose_win; //切换外观窗口
+
+signals:
 };
 #endif // MAINWINDOW_H
