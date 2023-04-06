b6ded030da2278d1d8ee71d8eec37856d4c7cc36
jsyhly <129362148+jsyhly@users.noreply.github.com>
2023-04-06 21:28:59 +0800

    Create mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <QLabel>
 #include <QMovie>
+#include "string.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -36,164 +37,18 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
                QMovie *backgroundMovie=new QMovie(this);
                srand((int)time(0));
                int i=rand()%12;
-               switch (i) {
-               case 0:
-                       backgroundMovie = new QMovie(":/resources/left_mouse_click/default3.gif",QByteArray(),this);
-                       backgroundMovie->setScaledSize(pet_appearance->size());
-                       connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                       if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                       {
-                          backgroundMovie->stop();
-                          pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                       }
-                       });
-                       pet_appearance->setMovie(backgroundMovie);
-                       backgroundMovie->start();
-                   break;
-               case 1:
-                       backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick9.gif",QByteArray(),this);
-                       backgroundMovie->setScaledSize(pet_appearance->size());
-                       connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                       if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                       {
-                          backgroundMovie->stop();
-                          pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                       }
-                       });
-                       pet_appearance->setMovie(backgroundMovie);
-                       backgroundMovie->start();
-                   break;
-               case 2:
-                       backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick16.gif",QByteArray(),this);
-                       backgroundMovie->setScaledSize(pet_appearance->size());
-                       connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                       if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                       {
-                          backgroundMovie->stop();
-                          pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                       }
-                       });
-                       pet_appearance->setMovie(backgroundMovie);
-                       backgroundMovie->start();
-                   break;
-               case 3:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick13.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 4:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick14.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 5:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick15.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 6:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick6.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 7:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick11.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 8:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick8.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 9:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick12.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 10:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick17.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                   pet_appearance->setMovie(backgroundMovie);
-                   backgroundMovie->start();
-                   break;
-               case 11:
-                   backgroundMovie = new QMovie(":/resources/left_mouse_click/leftclick18.gif",QByteArray(),this);
-                   backgroundMovie->setScaledSize(pet_appearance->size());
-                   connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
-                   if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
-                   {
-                      backgroundMovie->stop();
-                      pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
-                   }
-                   });
-                  pet_appearance->setMovie(backgroundMovie);
-                  backgroundMovie->start();
-                   break;
+               QString gif[12]={":/resources/left_mouse_click/default3.gif",":/resources/left_mouse_click/leftclick9.gif",":/resources/left_mouse_click/leftclick16.gif",":/resources/left_mouse_click/leftclick13.gif",":/resources/left_mouse_click/leftclick14.gif",":/resources/left_mouse_click/leftclick15.gif",":/resources/left_mouse_click/leftclick6.gif",":/resources/left_mouse_click/leftclick11.gif",":/resources/left_mouse_click/leftclick8.gif",":/resources/left_mouse_click/leftclick12.gif",":/resources/left_mouse_click/leftclick17.gif",":/resources/left_mouse_click/leftclick18.gif"};
+               backgroundMovie = new QMovie(gif[i],QByteArray(),this);
+               backgroundMovie->setScaledSize(pet_appearance->size());
+               connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
+               if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
+               {
+                  backgroundMovie->stop();
+                  pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
                }
+               });
+               pet_appearance->setMovie(backgroundMovie);
+               backgroundMovie->start();
 
            }
 }
