9f500079fbbddba8a18b9a4df9582c04e9bd4713
jsyhly <2421114789@qq.com>
2023-04-08 22:04:57 +0800

    Signed-off-by: jsyhly <2421114789@qq.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <cstdlib>
+#include <ctime>
+#include <QLabel>
+#include <QMovie>
+#include "string.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -24,3 +29,35 @@ void MainWindow::SelectAppearance(){
     pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
 }
 
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+               m_dragging = true;
+               m_startPos = event->globalPos() - frameGeometry().topLeft();
+               QMovie *backgroundMovie=new QMovie(this);
+               srand((int)time(0));
+               int i=rand()%12;
+               QString gif[12]={":/resources/left_mouse_click/default3.gif",":/resources/left_mouse_click/leftclick9.gif",":/resources/left_mouse_click/leftclick16.gif",":/resources/left_mouse_click/leftclick13.gif",":/resources/left_mouse_click/leftclick14.gif",":/resources/left_mouse_click/leftclick15.gif",":/resources/left_mouse_click/leftclick6.gif",":/resources/left_mouse_click/leftclick11.gif",":/resources/left_mouse_click/leftclick8.gif",":/resources/left_mouse_click/leftclick12.gif",":/resources/left_mouse_click/leftclick17.gif",":/resources/left_mouse_click/leftclick18.gif"};
+               backgroundMovie = new QMovie(gif[i],QByteArray(),this);
+               backgroundMovie->setScaledSize(pet_appearance->size());
+               connect(backgroundMovie, &QMovie::frameChanged, [=](int frameNumber){
+               if(frameNumber== backgroundMovie->frameCount() -1 )//currentFrameNumber()：获取当前帧数
+               {
+                  backgroundMovie->stop();
+                  pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
+               }
+               });
+               pet_appearance->setMovie(backgroundMovie);
+               backgroundMovie->start();
+
+           }
+}
+
+/*
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+  {
+      if (event->button() == Qt::LeftButton) {
+          m_dragging = false;
+           pet_appearance->setPixmap(QPixmap(":/resources/static/default.png").scaled(pet_appearance->size()));
+      }
+  }*/
